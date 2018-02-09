2e5b4b99ac6ef07bf891820da6c543c5660ebd58
Andrew Goodwin <Andy@Andrews-MBP.default>
2018-02-09 08:27:34 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('viewengine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page'
+		,welcomeMessage: 'This is a welcome message'
+	})
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	})
+});
+app.get('/bad', (req, res) => {
+	res.send({
+		error : 'Could not handle this request'
+	});
+});
+
+
+app.listen(3000,() => {
+	console.log('Server is up on port 3000');
+});
